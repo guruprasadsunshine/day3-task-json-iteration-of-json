4cc693fd93ae24a3600581a1548f80c2026185c3
guruprasadsunshine <110889295+guruprasadsunshine@users.noreply.github.com>
2022-10-19 19:05:33 +0530

    Add files via upload

---
+++ class folder/resumetask.js
@@ -0,0 +1,13 @@
+let resume = [{
+
+"Name":"Guru prasad",
+"From":"Bangalore",
+"Education":"B.com",
+ "job":"Graphic designer",
+ "Working in" :"Smiles.Ai",
+ "Hobbies":"Sports,Movies",
+ "DOB":"28/04/1993",
+ "living in":"Bangalore",
+ "skills":"Photoshop,illustrator",
+}]
+console.log(resume)
---
+++ class folder/taskday3.js
@@ -0,0 +1,36 @@
+let guru =[{
+"institute":"guvi",
+"city":"chennai",
+"bestmentor": "raghava kumar",
+"jetspeedmentor": "vidhya",
+"donttellher":"from guru",
+"mentor":"ragav kumar",
+}]
+
+
+for(var i=0; i<guru.length;i++){
+    console.log(guru[i])
+}
+
+for(hari in guru){
+    console.log(hari)
+console.log(guru[hari])
+}
+
+for(guvi of guru){
+console.log(guvi)
+}
+
+
+guru.forEach((g)=>{
+    console.log(g)
+})
+
+
+
+
+
+
+
+
+
